882f133e07beb237e78524194bc4bf9e7ab18f22
dkool <dennis.kool@cloudnation.nl>
2022-10-19 15:33:22 +0200

    feat: test output merged_ids

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,5 @@ output "agw" {
 }
 
 output "merged_ids" {
-  //value = concat(values(azurerm_public_ip.pip)[*].id, values(azurerm_key_vault.kv)[*].id, values(azurerm_application_gateway.application_gateway)[*].id)
-  value = values(azurerm_application_gateway.application_gateway)[*].id
+  value = concat(values(azurerm_public_ip.pip)[*].id, values(azurerm_key_vault.kv)[*].id, values(azurerm_application_gateway.application_gateway).id)
 }
