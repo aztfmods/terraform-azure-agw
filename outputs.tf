8608b8152d58e617a880b16b93d59c4668b5850c
dkool <dennis.kool@cloudnation.nl>
2022-10-19 15:43:30 +0200

    feat: test output merged_ids

--- outputs.tf
+++ outputs.tf
@@ -3,5 +3,5 @@ output "agw" {
 }
 
 output "merged_ids" {
-  value = concat(values(azurerm_public_ip.pip)[*].id, values(azurerm_key_vault.kv)[*].id, azurerm_application_gateway.application_gateway.id)
+  value = concat(azurerm_public_ip.pip.id, azurerm_key_vault.kv.id, azurerm_application_gateway.application_gateway.id)
 }
