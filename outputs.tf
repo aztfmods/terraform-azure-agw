93b2619349c3767fc952b67c731a7e8363a05084
dkool <dennis.kool@cloudnation.nl>
2022-10-19 14:54:01 +0200

    feat: add diagnostics integration

---
+++ outputs.tf
@@ -0,0 +1,7 @@
+output "agw" {
+  value = azurerm_application_gateway.application_gateway
+}
+
+output "merged_ids" {
+  value = concat(values(azurerm_public_ip.pip)[*].id, values(azurerm_key_vault.kv)[*].id, values(azurerm_application_gateway.application_gateway)[*].id)
+}
