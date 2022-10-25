5ea23bd78593b979a166805adcac72acdf11f7ce
dkool <dennis.kool@cloudnation.nl>
2022-10-25 12:08:12 +0200

    feat: change merged_ids output to list

--- outputs.tf
+++ outputs.tf
@@ -3,5 +3,5 @@ output "agw" {
 }
 
 output "merged_ids" {
-  value = azurerm_application_gateway.application_gateway.id
+  value = [azurerm_application_gateway.application_gateway.id, azurerm_public_ip.pip.id, azurerm_key_vault.kv.id]
 }
